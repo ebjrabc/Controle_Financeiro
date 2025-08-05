225191dcc024a5b2acc472ef4a126d725dcd415d
ebjrabc <62903421+ebjrabc@users.noreply.github.com>
2025-08-05 13:01:47 -0300

    Add files via upload

---
+++ modulo_cad_ativo.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+# 🖥️ Título da página
+st.set_page_config(page_title="Consulta de Ativos B3", layout="centered")
+
+# 🚧 Mensagem de manutenção
+st.markdown("""
+# ⚠️ Página em manutenção
+
+Estamos realizando melhorias no sistema.  
+Por favor, volte mais tarde.
+
+""", unsafe_allow_html=True)
